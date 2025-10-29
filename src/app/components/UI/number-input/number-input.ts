9a1f7200cfe3bdb96ed86347faddb0939356c0b6
Roberto Heckers <robertoheckers@gmail.com>
2025-10-29 15:32:30 +0100

    feat(list): improve debounced input

--- src/app/components/UI/number-input/number-input.ts
+++ src/app/components/UI/number-input/number-input.ts
@@ -1,7 +1,7 @@
 import { Component, input, model } from '@angular/core';
 import { outputFromObservable, toObservable } from '@angular/core/rxjs-interop';
 import { FormsModule } from '@angular/forms';
-import { debounceTime, distinctUntilChanged } from 'rxjs';
+import { debounce, distinctUntilChanged, filter, timer } from 'rxjs';
 
 @Component({
   selector: 'app-number-input',
@@ -16,8 +16,9 @@ export class NumberInput {
 
   valueChange = outputFromObservable(
     toObservable(this.value).pipe(
-      debounceTime(this.debounceTime()),
+      debounce(() => timer(this.debounceTime())),
       distinctUntilChanged(),
+      filter((v) => v <= this.maxValue()),
     ),
   );
 }
